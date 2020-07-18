2ed427608fbd08e4b54c6703b763bab385502665
bradchao <bk400288>
2020-07-18 16:25:54 +0800

    071802

---
+++ app/src/main/java/tw/brad/apps/mysign/MyView.java
@@ -0,0 +1,16 @@
+package tw.brad.apps.mysign;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyView extends View {
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setBackgroundColor(Color.GREEN);
+    }
+}
